9ce4dde8272a3bbcd299ceb6263c0a9a5f82ac48
nano <s.rahulnarayanan@gmail.com>
2025-11-11 12:42:25 +0530

    update the code

--- code/lib/supabase.ts
+++ code/lib/supabase.ts
@@ -5,9 +5,6 @@ let supabaseClient: ReturnType<typeof createBrowserClient> | null = null
 export function getSupabaseClient() {
   if (!supabaseClient) {
     supabaseClient = createBrowserClient(
-      "https://qksjuwwxwvurlvsxudln.supabase.co", // your Supabase project URL
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFrc2p1d3d4d3Z1cmx2c3h1ZGxuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIyNzgzMjIsImV4cCI6MjA3Nzg1NDMyMn0.mOsOsUKgZg9YeSa7yu2Wpqp-DNhhbmeoNIkgiybn9F8" // your anon key
-    )
-  }
-  return supabaseClient
+      "", // your Supabase project URL
+      ""
 }
